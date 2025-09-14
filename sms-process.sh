b23503b6ebf8189c49eb358514cf8a90791ef55d
Bartłomiej "Magnetic-Fox" Węgrzyn <magnetic-fox@hotmail.com>
2025-09-14 22:29:11 +0200

    Translate error messages to English

--- sms-process.sh
+++ sms-process.sh
@@ -23,11 +23,11 @@ SMSCMDPY='/etc/asterisk/scripts/smsCommand.py'
 SIPIMSND='/etc/asterisk/scripts/amiSendSIPIM.py'
 
 # Error texts presented to user (avoid non-ASCII characters!)
-CANTDELIVER='Nie udalo sie dostarczyc wiadomosci SMS do numeru'
+CANTDELIVER='Cannot deliver SMS to'
 
 # Error texts logged on server (whatever You wish)
-CANNOTSEND='Nie udało się wysłać wiadomości SMS do numeru'
-CANTDELTASK='Nie udało się bezpiecznie usunąć odebranej wiadomości SMS!'
+CANNOTSEND='Cannot send SMS to'
+CANTDELTASK='Could not safely delete received SMS!'
 
 
 
