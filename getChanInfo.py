c88dbea279cfad74869c7830c4d7a128a2e308d5
Magnetic-Fox <magnetic-fox@hotmail.com>
2025-11-26 22:02:40 +0100

    Remove stupid code and delete autorun section

--- getChanInfo.py
+++ getChanInfo.py
@@ -2,35 +2,23 @@
 
 # Channel information gathering utility
 #
-# by Magnetic-Fox, 19-20.04.2025, 07.09.2025
+# by Magnetic-Fox, 19.04.2025 - 26.11.2025
 #
 # (C)2025 Bartłomiej "Magnetic-Fox" Węgrzyn
 
-# Import section
-import sys
 import mysql.connector
 import sqlConfig
 
-# Some globals
-mydb = None
-cur = None
 
 # SQL connection preparation procedure
 def prepareSQLConnection():
-	global mydb, cur
+	mydb = mysql.connector.connect(**sqlConfig.config)
+	cur = mydb.cursor(buffered = True)
 
-	if mydb == None:
-		mydb = mysql.connector.connect(**sqlConfig.config)
-
-	if cur == None:
-		cur = mydb.cursor(buffered = True)
-
-	return
+	return mydb, cur
 
 # SQL connection freeing procedure
-def freeSQLConnection():
-	global mydb, cur
-
+def freeSQLConnection(mydb, cur):
 	if(cur != None):
 		cur.close()
 		cur = None
@@ -43,32 +31,14 @@ def freeSQLConnection():
 
 # Get extension and desired service information
 def getChanInfo(number):
-	global mydb, cur
-
-	prepareSQLConnection()
+	mydb, cur = prepareSQLConnection()
 	sql = "SELECT Extension, Message FROM PBXNumbers WHERE Account=%s"
 	cur.execute(sql, (number,))
 	res = cur.fetchall()
-	freeSQLConnection()
+	freeSQLConnection(mydb, cur)
 
 	try:
 		return (res[0][0], res[0][1])
 
 	except:
 		return None
-
-# Autorun
-if __name__ == "__main__":
-	if len(sys.argv) > 1:
-		resp = getChanInfo(sys.argv[1])
-
-		if(resp != None):
-			print(resp[0])
-			print(resp[1])
-			exit(0)
-
-		else:
-			exit(1)
-
-	else:
-		exit(1)
