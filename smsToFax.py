71457a8242b2bbd40568fa1da963c0fd2be221bf
Magnetic-Fox <magnetic-fox@hotmail.com>
2025-11-28 14:45:24 +0100

    Clean up code: make use of new functions in callFileTools and don't create temporary directory for conversions

--- smsToFax.py
+++ smsToFax.py
@@ -2,45 +2,38 @@
 
 # SMS to fax processor
 #
-# by Magnetic-Fox, 19.04.2025 - 27.11.2025
+# by Magnetic-Fox, 19.04.2025 - 28.11.2025
 #
 # (C)2025 Bartłomiej "Magnetic-Fox" Węgrzyn
 
-import subprocess
-import tempfile
-import os
 import datetime
 import cutter
 import tiffTools
-import callFileGenerator
+import callFileTools
 import smsSuiteConfig
 import suiteLogger
 
 
-# Simple name generation helper
-def generateDateTimeName(prefix = "", postfix = "", date = None):
-	if date == None:
-		date = datetime.datetime.now()
-	return prefix + date.strftime("%Y-%m-%d-%H-%M-%S-%f") + postfix
-
 # Asterisk call file creation utility (depends on settings from smsSuiteConfig)...
 def generateCallFile(toExtension, tiffFilePath, callFileName):
+	# Prepare additional variables
 	faxOptVariables = [	"FAXOPT(localstationid)=" + smsSuiteConfig.FAXID,
 				"FAXOPT(ecm)=" + ("yes" if smsSuiteConfig.FAX_USE_ECM else "no"),
 				"FAXOPT(minrate)=" + str(smsSuiteConfig.FAX_MIN_RATE),
 				"FAXOPT(maxrate)=" + str(smsSuiteConfig.FAX_MAX_RATE),
 				"FAXOPT(headerinfo)=" + smsSuiteConfig.FAXHEADER	]
 
-	callFileGenerator.generateCallFile(	callFileName,
-						toExtension,
-						smsSuiteConfig.CALLER_ID_FAX,
-						"SendFax",
-						tiffFilePath + ",F",
-						faxOptVariables,
-						smsSuiteConfig.MAX_RETRIES_FAX,
-						smsSuiteConfig.RETRY_TIME_FAX,
-						smsSuiteConfig.WAIT_TIME_FAX,
-						smsSuiteConfig.ARCHIVE_CALL_FILE_FAX	)
+	# Generate call file
+	callFileTools.generateCallFile(	callFileName,
+					toExtension,
+					smsSuiteConfig.CALLER_ID_FAX,
+					"SendFax",
+					tiffFilePath + ",F",
+					faxOptVariables,
+					smsSuiteConfig.MAX_RETRIES_FAX,
+					smsSuiteConfig.RETRY_TIME_FAX,
+					smsSuiteConfig.WAIT_TIME_FAX,
+					smsSuiteConfig.ARCHIVE_CALL_FILE_FAX	)
 
 	return
 
@@ -62,42 +55,29 @@ def generateHeader(fromNumber, dateTime, messageReference):
 # All processing utility...
 def process(fromNumber, toNumber, toExtension, message, dateTime, messageReference):
 	try:
-		# Prepare temporary directory
-		oldDir = os.getcwd()
-		dir = tempfile.TemporaryDirectory()
-		os.chdir(dir.name)
-
 		# Generate file names (to which number + date and time)
-		tiffFileName = generateDateTimeName(str(toNumber) + "-", ".tiff")
-		callFileName = generateDateTimeName(str(toNumber) + "-", ".call")
+		tiffFileName = callFileTools.generateDateTimeName(str(toNumber) + "-", ".tiff")
+		callFileName = callFileTools.generateDateTimeName(str(toNumber) + "-", ".call")
 
 		# Convert prepared text (header + message) to the G3 TIFF file
-		tiffTools.textToTIFF(	tiffFileName,
+		tiffTools.textToTIFF(	smsSuiteConfig.FAX_IMG_DIR + "/" + tiffFileName,
 					generateHeader(fromNumber, dateTime, messageReference) + message,
 					smsSuiteConfig.FAX_RESOLUTION,
 					smsSuiteConfig.FAX_FONT,
 					smsSuiteConfig.FAX_TOP_MARGIN	)
 
 		# Crop unnecessary white part (makes less fax recording paper waste)
-		cutter.loadAndCrop(tiffFileName, cutter.calculateCutMargin(smsSuiteConfig.FAX_RESOLUTION, smsSuiteConfig.FAX_CUTTER_VALUE))
-
-		# Move prepared fax page (G3 TIFF) to outgoing faxes directory
-		subprocess.run(["mv", tiffFileName, smsSuiteConfig.FAX_IMG_DIR])
+		cutter.loadAndCrop(	smsSuiteConfig.FAX_IMG_DIR + "/" + tiffFileName,
+					cutter.calculateCutMargin(smsSuiteConfig.FAX_RESOLUTION, smsSuiteConfig.FAX_CUTTER_VALUE)	)
 
 		# Generate call file
-		generateCallFile(toExtension, smsSuiteConfig.FAX_IMG_DIR + "/" + tiffFileName, callFileName)
+		generateCallFile(toExtension, smsSuiteConfig.FAX_IMG_DIR + "/" + tiffFileName, smsSuiteConfig.AST_TEMP_SPOOL + "/" + callFileName)
 
-		# Move call file to the Asterisk's temporary spool folder (which should definitely be on the same disk 'outgoing' directory is)
-		subprocess.run(["mv", callFileName, smsSuiteConfig.AST_TEMP_SPOOL])
-
-		# Move call file to the Asterisk's 'outgoing' directory
-		subprocess.run(["mv", smsSuiteConfig.AST_TEMP_SPOOL + "/" + callFileName, smsSuiteConfig.ASTERISK_SPOOL])
+		# Move call file to the Asterisk's outgoing directory
+		callFileTools.moveCallFile(callFileName, smsSuiteConfig.AST_TEMP_SPOOL, smsSuiteConfig.ASTERISK_SPOOL)
 
 	except Exception as e:
+		# Log any errors
 		suiteLogger.logError(str(e))
-
-	finally:
-		os.chdir(oldDir)
-		dir.cleanup()
 
 	return
